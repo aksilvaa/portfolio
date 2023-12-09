9fd06d271f629161dee1ab589ec808408f236945
Alison Keuver da Silva <alison.keuver@totvs.com.br>
2023-12-09 11:07:50 -0300

    update angular version, and fix errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
-import { CompetencesComponent } from './competences/competences.component';
-import { EducationComponent } from './education/education.component';
-import { ProjectsComponent } from './projects/projects.component';
 
-
-const routes: Routes = [{path:'',component:AppComponent},{path:'competences',component:CompetencesComponent},{path:'projects',component:ProjectsComponent},{path:'cv',component:EducationComponent},{path:'about',component:AboutComponent}];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
